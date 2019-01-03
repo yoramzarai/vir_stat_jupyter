e5e95147dfebad7b5d13ffb0db409a29e0ddb30a
yoramzarai <yoram.zarai@gmail.com>
2019-01-03 23:11:13 +0200

    added starcode clustering template

--- mysequtils.py
+++ mysequtils.py
@@ -1,3 +1,5 @@
+from Bio import SeqIO
+
 def seq_create_fasta(seq, labels=False):
     '''Given a sequence of strings and correponding labels, return a string containing 
     the FASTA data in the form of:
@@ -12,5 +14,21 @@ def seq_create_fasta(seq, labels=False):
     return ''.join([s+'\n' if f else '> '+l+'\n' for s,l in zip(seq, labels) for f in range(2)])
 
     
-    
-    
+def seq2plain_text_file(seq, fname):
+    '''This function writes the input list of sequences to a file, where
+    each sequence is written in a separate line.'''
+    with open(fname, 'wt') as fout: print(*seq, sep='\n', file=fout)
+
+
+def my_fasta_read(fname):
+    '''Reads fasta file. 
+    The functions returns a list of the sequences, a list of the sequences IDs 
+    and a list of the sequences descriptions (content of the text following >>)'''
+    seq = []
+    iD = []
+    desc = []  # description 
+    for record in SeqIO.parse(fname, "fasta"):
+        desc.append(record.description)
+        seq.append(str(record.seq))
+        iD.append(record.id)
+    return seq, iD, desc
